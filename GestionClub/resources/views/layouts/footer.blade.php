7fc4313f76bcac7bbdab1fc17945b733e0934a38
Joanna Pittman <maohea@email.com>
2024-03-07 15:06:10 -1000

    First Push Laravel

---
+++ GestionClub/resources/views/layouts/footer.blade.php
@@ -0,0 +1,16 @@
+<footer class="app-footer">
+    <div class="container">
+        <!--Copyright-->
+        <div >
+            <div class="container-fluid text-center">
+                Copyright &copy; <?php echo date('Y'); ?> <a
+                    href="https://lycee2pirae.com/" target="_blank">
+                    <b>Lycée Diadème, Te Tara O Maiao (Pirae)</b></a>
+            </div>
+            <div class="container-fluid text-center"><a
+                    href="/mentionLegale" target="_blank">
+                    <b>Mentions Légales</b></a>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ GestionClub/resources/views/pages/apropos.blade.php
@@ -0,0 +1,7 @@
+@extends("layouts.master")
+@section('title')
+    A propos
+@endsection
+@section('content')
+    <h1>A propos</h1>
+@endsection
